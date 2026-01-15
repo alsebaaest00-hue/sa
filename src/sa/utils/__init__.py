60ed2d6ad867cce48f4061886feca0a744561be5
alsebaaest00-hue <alsebaaest00@gmail.com>
2026-01-15 22:35:00 +0000

    refactor: update imports and format code

--- src/sa/utils/__init__.py
+++ src/sa/utils/__init__.py
@@ -2,5 +2,15 @@
 
 from .config import config, Config
 from .suggestions import SuggestionEngine
+from .database import db, Database
+from .projects import project_manager, ProjectManager
 
-__all__ = ["config", "Config", "SuggestionEngine"]
+__all__ = [
+    "config",
+    "Config",
+    "SuggestionEngine",
+    "db",
+    "Database",
+    "project_manager",
+    "ProjectManager",
+]
